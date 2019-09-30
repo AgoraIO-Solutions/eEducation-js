290add6ffe5549af8890e3c98a582b647151122f
Philip Futernik <philip@agora.io>
2019-09-30 18:22:00 -0400

    Initial commit.

---
+++ scripts/firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCSdT-N8yt1m-70C1yosjsnCmYWThHteBs",
+    authDomain: "stud2-dd8aa.firebaseapp.com",
+    databaseURL: "https://stud2-dd8aa.firebaseio.com",
+    projectId: "stud2-dd8aa",
+    storageBucket: "",
+    messagingSenderId: "308354611054",
+    appId: "1:308354611054:web:e4518b1c45fb5f92"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let database = firebase.database();
